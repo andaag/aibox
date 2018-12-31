43226d41df2ab6799dcad3a109221ceeb2fb8680
Anders Aagaard <aagaande@gmail.com>
2018-12-31 11:53:02 +0100

    switching to jupyter lab by default

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -m
 
 ldconfig
 echo "Executing as $1"
-sudo -s -H -u $1 env "PATH=$PATH" jupyter notebook --ip 0.0.0.0 --NotebookApp.password='sha1:a60ff295d0b9:506732d050d4f50bfac9b6d6f37ea6b86348f4ed' &
+sudo -s -H -u $1 env "PATH=$PATH" jupyter lab --ip 0.0.0.0 --NotebookApp.password='sha1:a60ff295d0b9:506732d050d4f50bfac9b6d6f37ea6b86348f4ed' &
 sleep 0.5
 rm -f /usr/bin/sudo
 fg
